4716226e7463f5e8acd284bcec92c834d97eea3d
frejala <fa.rejala03@gmail.com>
2024-08-13 13:53:35 -0400

    Add Hero component

---
+++ src/constants/index.ts
@@ -0,0 +1,2 @@
+export const HERO_CONTENT =
+  'I like to craft solid and scalable frontend experiences with React and Tailwind CSS. I also enjoy building backend services with Springboot, Django and FastAPI.';
