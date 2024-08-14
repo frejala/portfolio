e152b4e67d9d1159c1eeba3d9af3b7a1c45e8434
frejala <fa.rejala03@gmail.com>
2024-08-13 23:22:18 -0400

    Add About component and update constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,5 @@
 export const HERO_CONTENT =
-  'I like to craft solid and scalable frontend experiences with React and Tailwind CSS. I also enjoy building backend services with Springboot, Django and FastAPI.';
+  'I like to craft solid and scalable frontend experiences with React and Tailwind CSS. I also enjoy building backend services with Spring Boot, Django and FastAPI.';
+
+export const ABOUT_TEXT =
+  " Hi, I'm Fabrizio Rejala, a passionate developer based in Paraguay. I love creating websites with an excellent user experience. Currently, I am studying Computer Science Engineering. During my time at university, I have worked on various projects involving automation, project management, algorithm optimization, and databases. I have extensive experience in frontend development with frameworks such as React and Angular. Additionally, I have backend development experience with technologies like FastAPI, Django, and Spring Boot. Throughout my career, I have been recognized for my ability to solve complex problems with high-quality software solutions. My goal is to become a highly experienced developer and elevate the user experience to the next level.";
